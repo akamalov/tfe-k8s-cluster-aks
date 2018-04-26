5225eb0cc509159d56149b9de596ba35a4ea3df6
Roger Berlind <roger@hashicorp.com>
2018-04-25 21:14:25 -0400

    fixed part of az install

--- main.tf
+++ main.tf
@@ -83,13 +83,7 @@ resource "azurerm_kubernetes_cluster" "k8sexample" {
 
 resource "null_resource" "install_az" {
   provisioner "local-exec" {
-    command = "export AZ_REPO=$(lsb_release -cs)"
-  }
-  provisioner "local-exec" {
-    command = "echo $AZ_REPO"
-  }
-  provisioner "local-exec" {
-    command = "echo \"deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main\" | sudo tee /etc/apt/sources.list.d/azure-cli.list"
+    command = "echo \"deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main\" | sudo tee /etc/apt/sources.list.d/azure-cli.list"
   }
   provisioner "local-exec" {
     command = "sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893"
